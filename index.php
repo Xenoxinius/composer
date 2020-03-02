3cec06dae8ffe318c215001a1020b4177d8223fe
Becode <stevekeersma@gmail.com>
2020-03-02 16:00:35 +0100

    buttons working correctly

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
@@ -10,19 +10,51 @@ require_once 'vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\BrowserConsoleHandler;
 use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
-use Monolog\Handler\MailHandler;
+use Monolog\Handler\NativeMailerHandler;
 
 // Create the logger
+// NOTICE AND INFO ARE ALSO PART OF DEBUG SO ONLY NEED ONE LINE
 $logger = new Logger('my_logger');
+$logger->pushHandler(new BrowserConsoleHandler(Logger::DEBUG));
+$logger->pushHandler(new StreamHandler(__DIR__ . '/logs/info.log', Logger::DEBUG));
+
+
 // Now add some handlers
-$logger->pushHandler(new BrowserConsoleHandler(Logger::DEBUG, false));
-$logger->pushHandler(new BrowserConsoleHandler(Logger::INFO, false));
-$logger->pushHandler(new BrowserConsoleHandler(Logger::NOTICE, false));
-$logger->pushHandler(new StreamHandler(__DIR__.'/warning.log', Logger::WARNING));
-$logger->pushHandler(new FirePHPHandler());
+if (isset($_GET['type'])) {
+    if ($_GET['type'] == 'DEBUG') {
+        $logger->debug($_GET['message'] ?? "");
+    } elseif ($_GET['type'] == 'INFO') {
+        $logger->info($_GET['message'] ?? "");
+    } elseif ($_GET['type'] == 'NOTICE') {
+        $logger->notice($_GET['message'] ?? "");
+    };
+
+
+    if ($_GET['type'] == 'WARNING') {
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/logs/warning.log', Logger::WARNING));
+        $logger->warning($_GET['message'] ?? "");
+    }
+
+
+    if ($_GET['type'] == 'ERROR') {
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/logs/warning.log', Logger::ERROR));
+        $logger->pushHandler(new NativeMailerHandler('yolo@gmail.com', 'poggers', 'matthijs'));
+        $logger->error($_GET['message'] ?? "");
+    } elseif ($_GET['type'] == 'CRITICAL') {
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/logs/warning.log', Logger::CRITICAL));
+        $logger->critical($_GET['message'] ?? "");
+    }elseif ($_GET['type'] == 'ALERT') {
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/logs/warning.log', Logger::ALERT));
+        $logger->critical($_GET['message'] ?? "");
+    }
+
+
+    if ($_GET['type'] == 'EMERGENCY') {
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/logs/emergency.log', Logger::EMERGENCY));
+        $logger->emergency($_GET['message'] ?? "");
+    }
+
+}
 
 // You can now use your logger
-$logger->info($_GET['message']);
-
 require_once 'buttons.html';
